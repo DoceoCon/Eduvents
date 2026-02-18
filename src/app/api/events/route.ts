b0fce065f63efb0b14cfddfeb379593ca5a31628
Ahmad <mac2019@Ahmad-Kamran.local>
2026-02-18 15:40:55 +0500

    remove shippting address

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -171,9 +171,7 @@ export async function POST(req: NextRequest) {
           mode: "payment",
           automatic_tax: { enabled: true },
           billing_address_collection: "required",
-          shipping_address_collection: {
-            allowed_countries: ["GB"],
-          },
+          allow_promotion_codes: true,
           success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/list-event?success=true&session_id={CHECKOUT_SESSION_ID}&event_id=${newEvent._id}`,
           cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}/list-event?canceled=true&event_id=${newEvent._id}`,
           metadata: {
